c6887eb50da000edf87d80c0a3900a11acb48312
alkimides <inaki.seim@gmail.com>
2019-12-03 00:26:34 +0100

    hola

---
+++ WebApplication2/Pages/partidospoliticos.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.models;
+
+namespace WebApplication2.Pages
+{
+    public class partidospoliticosModel : PageModel
+    {
+
+        public List<partidosPoliticos> partidospoliticos { get; set; }
+        public void OnGet()
+        {
+
+            partidospoliticos = new List<partidosPoliticos>
+            {
+
+                new partidosPoliticos()
+                {
+                    Nombre = "Partido Popular",
+                    Siglas = "PP",
+                    Descripcion="corrupcion",
+                    Posicion = "centro-derecha",
+                    Votantes = "5.000.000"
+                },
+
+                new partidosPoliticos()
+                {
+                    Nombre = "Partido Socialista Obrero Español",
+                    Siglas = "PSOE",
+                    Descripcion="Partido para tontos",
+                    Posicion = "centro-izquierda",
+                    Votantes = "6.700.000"
+                },
+
+                new partidosPoliticos()
+                {
+                    Nombre = "VOX",
+                    Siglas = "VOX",
+                    Descripcion="TOP",
+                    Posicion = "derecha",
+                    Votantes = "3.600.000"
+                }
+
+            };
+
+        }
+    }
+}
---
+++ WebApplication2/models/partidosPoliticos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.models
+{
+    public class partidosPoliticos
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Siglas { get; set; }
+        public string Descripcion { get; set; }
+        public string Posicion { get; set; }
+        public string Votantes { get; set; }
+    }
+
+}
